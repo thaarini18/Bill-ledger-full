f6019eeec504bed23a24212ea736ac144edaf3d6
mac <mac@Shugandhiga.local>
2026-03-13 09:41:33 +0530

    deployment fix

--- app.py
+++ app.py
@@ -326,4 +326,4 @@ def reset_ledger():
 # ===========================================================================
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    pp.run(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=port)
