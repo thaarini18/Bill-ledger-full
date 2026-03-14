32d88880b7b5afece35f8cc8cdd22bb098834221
mac <mac@Shugandhiga.local>
2026-03-14 18:24:47 +0530

    deployment config

--- app.py
+++ app.py
@@ -284,5 +284,5 @@ def reset_ledger():
     return jsonify({"message": "Ledger reset successfully."})
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5001))
+    port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
